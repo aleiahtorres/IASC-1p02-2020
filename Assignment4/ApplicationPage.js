564066867f45b6fbbca3d22eb5e6a0ea4138845b
aleiahtorres <at19gf@brocku.ca>
2020-12-14 12:29:44 -0500

    commit

---
+++ Assignment4/ApplicationPage.js
@@ -0,0 +1,13 @@
+function addWords(){
+  var words=document.getElementById("Name").value;
+  var words=document.getElementById("Email").value;
+  var words=document.getElementById("Phone").value;
+
+console.log(addWords);
+
+  var words=document.getElementById("Yes").value;
+  var words=document.getElementById("No").value;
+
+  var words=document.getElementById("goal1").value;
+  var words=document.getElementById("goal2").value;
+}
