e01a20c19a0b7ae73ec8085c25d180fc3386ee98
aleiahtorres <at19gf@brocku.ca>
2020-12-20 01:23:33 -0500

    js

--- Assignment4/ApplicationPage.js
+++ Assignment4/ApplicationPage.js
@@ -1,13 +1,6 @@
-function addWords(){
-  var words=document.getElementById("Name").value;
-  var words=document.getElementById("Email").value;
-  var words=document.getElementById("Phone").value;
 
-console.log(addWords);
+//A Function to printout Thank you! when called
 
-  var words=document.getElementById("Yes").value;
-  var words=document.getElementById("No").value;
-
-  var words=document.getElementById("goal1").value;
-  var words=document.getElementById("goal2").value;
+function pushMe(){
+	document.getElementById("output").innerHTML="Thank you!";
 }
---
+++ Assignment4/ContactPage.js
@@ -0,0 +1,5 @@
+//A Function to printout Thank you! when called
+
+function pushMe(){
+	document.getElementById("output").innerHTML="Thank you!";
+}
